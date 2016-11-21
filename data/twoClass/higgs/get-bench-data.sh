f024e5f36a7d3ac17cb76696ace9b2ef516a0186
ledell <erin@h2o.ai>
2016-11-21 08:37:38 +0000

    Updated higgs/get-bench-data.sh

--- data/twoClass/higgs/get-bench-data.sh
+++ data/twoClass/higgs/get-bench-data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # A few subsets of the original HIGGS.csv file
-curl -O https://s3.amazonaws.com/erin-data/higgs/higgs_test_1k.csv  #first 1k rows of HIGGS.csv 
+curl -O https://s3.amazonaws.com/erin-data/higgs/higgs_train_1k.csv  #first 1k rows of HIGGS.csv 
 curl -O https://s3.amazonaws.com/erin-data/higgs/higgs_train_10k.csv
 curl -O https://s3.amazonaws.com/erin-data/higgs/higgs_train_100k.csv
 curl -O https://s3.amazonaws.com/erin-data/higgs/higgs_train_1M.csv

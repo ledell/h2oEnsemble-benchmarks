978cf1e2611e96baa424f5c430caf98f38fc10be
ledell <erin@h2o.ai>
2016-11-21 03:16:40 +0000

    Changed wget to curl in higgs data script

--- data/twoClass/higgs/get-bench-data.sh
+++ data/twoClass/higgs/get-bench-data.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # A few subsets of the original HIGGS.csv file
-wget https://s3.amazonaws.com/uciml-higgs/higgs_1k.csv # first 1k rows of HIGGS.csv 
-wget https://s3.amazonaws.com/uciml-higgs/higgs_10k.csv
-wget https://s3.amazonaws.com/uciml-higgs/higgs_100k.csv
-wget https://s3.amazonaws.com/uciml-higgs/higgs_1M.csv
+curl -O https://s3.amazonaws.com/uciml-higgs/higgs_1k.csv # first 1k rows of HIGGS.csv 
+curl -O https://s3.amazonaws.com/uciml-higgs/higgs_10k.csv
+curl -O https://s3.amazonaws.com/uciml-higgs/higgs_100k.csv
+curl -O https://s3.amazonaws.com/uciml-higgs/higgs_1M.csv
 
 # The last 500k observations of HIGGS.csv are the designated test set.
-wget https://s3.amazonaws.com/uciml-higgs/higgs_test.csv
-wget https://s3.amazonaws.com/uciml-higgs/labels_higgs_test.csv
+curl -O https://s3.amazonaws.com/uciml-higgs/higgs_test.csv
+#curl -O https://s3.amazonaws.com/uciml-higgs/labels_higgs_test.csv
